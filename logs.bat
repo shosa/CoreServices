d53750b3d5c6e5105ff2d02000ce0ec3021d42ff
Shosa <kishosa@me.com>
2025-10-19 23:56:55 +0200

    bats: rimossi

--- logs.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-echo ============================================
-echo CoreServices Logs (Ctrl+C to exit)
-echo ============================================
-docker-compose -p coreservices logs -f
